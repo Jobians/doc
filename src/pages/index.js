7295000b789ef53bb726b7230d13d68778021f94
Jobians Techie <88005779+Jobians@users.noreply.github.com>
2022-05-25 20:27:06 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="https://t.me/jobianstechie">
-            Contact Me Now ✉️
+            Contact Me Now 
           </Link>
         </div>
       </div>
